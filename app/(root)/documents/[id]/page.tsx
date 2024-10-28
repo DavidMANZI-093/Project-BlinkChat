abffcd8c4ecf208f4348a7ee63d64b0891e78848
David MANZI <manzidavid093@gmail.com>
2024-10-28 13:09:21 +0200

    ...

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Editor } from '@/components/editor/Editor';
+import Header from '@/components/ui/Header';
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2"></div>
+      </Header>
       <Editor />
     </div>
   )
