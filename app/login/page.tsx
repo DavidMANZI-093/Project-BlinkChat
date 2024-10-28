abb24530ced1f727c34872408039fa34b1090f48
David MANZI <manzidavid093@gmail.com>
2024-10-28 09:23:36 +0200

    ...

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <main></main>
+  )
+}
+
+export default LoginPage
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <main></main>
+  )
+}
+
+export default SignUpPage
